bd339d3e750226e592c583ec65959adbd2a9b88f
Ted Gould <ted@gould.cx>
2012-10-24 15:43:21 +0200

    Converting to HUD package

--- debian/source_hud.py
+++ debian/source_hud.py
@@ -3,4 +3,4 @@ import os.path
 def add_info(report):
 	if not apport.packaging.is_distro_package(report['Package'].split()[0]):
 		report['ThirdParty'] = 'True'
-		report['CrashDB'] = 'indicator_appmenu'
+		report['CrashDB'] = 'hud'
