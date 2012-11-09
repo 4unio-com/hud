085e8280b7c6e82ad03521c629c1cef928a0e2f4
Ted Gould <ted@gould.cx>
2012-11-09 16:07:02 -0600

    Makes the HudQuery have a Dbus interface

--- src/hudquery.c
+++ src/hudquery.c
@@ -19,6 +19,7 @@
 #define G_LOG_DOMAIN "hudquery"
 
 #include "hudquery.h"
+#include "hud-query-iface.h"
 
 #include "hudresult.h"
 
@@ -60,7 +61,11 @@ struct _HudQuery
 
 typedef GObjectClass HudQueryClass;
 
-G_DEFINE_TYPE (HudQuery, hud_query, G_TYPE_OBJECT)
+static void query_dbus_iface_init (HudQueryIfaceComCanonicalHud * iface);
+
+G_DEFINE_TYPE_WITH_CODE (HudQuery, hud_query, G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (HUD_QUERY_IFACE_TYPE_COM_CANONICAL_HUD, query_dbus_iface_init))
+
 static guint hud_query_changed_signal;
 
 static HudQuery *last_created_query;
@@ -180,6 +185,15 @@ hud_query_class_init (HudQueryClass *class)
                                            NULL, g_cclosure_marshal_VOID__VOID, G_TYPE_NONE, 0);
 
   class->finalize = hud_query_finalize;
+}
+
+/* Connects to the dbus calls from the bus */
+static void
+query_dbus_iface_init (HudQueryIfaceComCanonicalHud * iface)
+{
+	/* TODO */
+
+	return;
 }
 
 /**
