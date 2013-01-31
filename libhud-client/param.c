d585416eca67623c841e44351493c28313e825f8
Ted Gould <ted@gould.cx>
2013-01-31 19:16:58 +0000

    Write out the 'start' event to the base action

--- libhud-client/param.c
+++ libhud-client/param.c
@@ -90,6 +90,60 @@ hud_client_param_finalize (GObject *object)
 	return;
 }
 
+/* Handle the async callback from the DBus call and make sure it
+   prints out something so we can track it */
+static void
+state_write_cb (GObject * object, GAsyncResult * res, gpointer user_data)
+{
+	const gchar * call = (const gchar *)user_data;
+	g_debug("Call '%s' completed", call);
+
+	GError * error = NULL;
+	GVariant * ret = g_dbus_connection_call_finish(G_DBUS_CONNECTION(object), res, &error);
+
+	if (error != NULL) {
+		g_warning("Unable to call '%s' on action: %s", call, error->message);
+	}
+
+	if (ret != NULL) {
+		g_variant_unref(ret);
+	}
+
+	return;
+}
+
+/* Writes to the base action a particular state */
+static void
+action_write_state (HudClientParam * param, const gchar * action)
+{
+	/* This shows that they're not interested in these events,
+	   which is fine, perhaps a bit lonely, but fine. */
+	if (param->priv->base_action == NULL || param->priv->base_action[0] == '\0') {
+		return;
+	}
+
+	GVariantBuilder tuple;
+	g_variant_builder_init(&tuple, G_VARIANT_TYPE_TUPLE);
+	g_variant_builder_add_value(&tuple, g_variant_new_string(action));
+	g_variant_builder_add_value(&tuple, g_variant_new_string(""));
+	g_variant_builder_add_value(&tuple, g_variant_new_array(G_VARIANT_TYPE_VARIANT, NULL, 0));
+
+	g_dbus_connection_call(param->priv->session,
+	                       param->priv->dbus_address,
+	                       param->priv->action_path,
+	                       "org.gtk.Actions",
+	                       "Activate",
+	                       g_variant_builder_end(&tuple),
+	                       NULL, /* return value */
+	                       G_DBUS_CALL_FLAGS_NONE,
+	                       -1, /* default timeout */
+	                       NULL, /* cancellable */
+	                       state_write_cb,
+	                       (gpointer)action);
+
+	return;
+}
+
 /**
  * hud_client_param_new:
  * @dbus_address: The address on dbus to find the actions
@@ -117,6 +171,8 @@ hud_client_param_new (const gchar * dbus_address, const gchar * base_action, con
 	param->priv->action_path = g_strdup(action_path);
 	param->priv->model_path = g_strdup(model_path);
 	param->priv->model_section = model_section;
+
+	action_write_state(param, "start");
 
 	return param;
 }
