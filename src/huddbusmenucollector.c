86fad0c227b422030b67c2d16d758406597559d1
Ted Gould <ted@canonical.com>
2013-02-05 03:52:27 +0000

    Making sure that dbusmenu collector has the right function.  Probably won't use it, but why not.

--- src/huddbusmenucollector.c
+++ src/huddbusmenucollector.c
@@ -747,6 +747,9 @@ hud_dbusmenu_collector_new_for_window (AbstractWindow  *window,
 #ifdef HAVE_BAMF
   collector->xid = bamf_window_get_xid (window);
 #endif
+#ifdef HAVE_HYBRIS
+  collector->xid = _ubuntu_ui_session_properties_get_window_id(window);
+#endif
   collector->keyword_mapping = hud_keyword_mapping_new();
   hud_keyword_mapping_load(collector->keyword_mapping, collector->application_id, DATADIR, GNOMELOCALEDIR);
 
