c4fbf6998f0472ed85b724006b072fc0c202fcaf
Ted Gould <ted@gould.cx>
2013-02-11 16:25:41 -0600

    Calling the DBus method with an appropriate string

--- libhud-client/query.c
+++ libhud-client/query.c
@@ -579,6 +579,15 @@ hud_client_query_execute_param_command (HudClientQuery * cquery, GVariant * comm
 	return param;
 }
 
+/* Strings to send over DBus for the toolbar items */
+static const gchar * item_strings[] = {
+	/* HUD_CLIENT_QUERY_TOOLBAR_FULLSCREEN */    "fullscreen",
+	/* HUD_CLIENT_QUERY_TOOLBAR_HELP */          "help",
+	/* HUD_CLIENT_QUERY_TOOLBAR_PREFERENCES */   "preferences",
+	/* HUD_CLIENT_QUERY_TOOLBAR_QUIT */          "quit",
+	/* HUD_CLIENT_QUERY_TOOLBAR_UNDO */          "undo",
+};
+
 /**
  * hud_client_query_execute_toolbar_item:
  * @cquery: A #HudClientQuery
@@ -593,6 +602,7 @@ hud_client_query_execute_toolbar_item (HudClientQuery * cquery, HudClientQueryTo
 {
 	g_return_if_fail(HUD_CLIENT_IS_QUERY(cquery));
 
+	_hud_query_com_canonical_hud_query_call_execute_toolbar_sync(cquery->priv->proxy, item_strings[item], timestamp, NULL, NULL);
 
 	return;
 }
