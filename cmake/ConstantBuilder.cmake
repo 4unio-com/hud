742a5b7d1fde6ddb2ed2a93f741c9520d333a27c
Pete Woods <pete.woods@canonical.com>
2013-03-18 20:15:57 +0000

    Make create-db.c react to changes in create-db.sql

--- cmake/ConstantBuilder.cmake
+++ cmake/ConstantBuilder.cmake
@@ -3,17 +3,17 @@ if(POLICY CMP0011)
   cmake_policy(SET CMP0011 NEW)
 endif(POLICY CMP0011)
 
-#FIXME This is not a stable macro (i.e. it won't do nothing on un-changed input)
 macro(add_constant_template outfiles name const_name input)
+  set(file_target "${CMAKE_CURRENT_BINARY_DIR}/${name}.c")
   add_custom_command(
-    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${name}.h" "${CMAKE_CURRENT_BINARY_DIR}/${name}.c"
-    COMMAND echo "\#include \"${name}.h\"" > "${CMAKE_CURRENT_BINARY_DIR}/${name}.c"
-    COMMAND echo -n "const gchar * ${const_name} = \"" >> "${CMAKE_CURRENT_BINARY_DIR}/${name}.c"
-    COMMAND cat "${input}" | xargs printf "%s " >> "${CMAKE_CURRENT_BINARY_DIR}/${name}.c"
-    COMMAND echo "\";" >> "${CMAKE_CURRENT_BINARY_DIR}/${name}.c"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    DEPENDS ${ARGN} "${input}"
-    VERBATIM
+    OUTPUT ${file_target}
+    COMMAND ${CMAKE_COMMAND}
+        "-Dname=${name}"
+        "-Dfile_target=${file_target}"
+        "-Dconst_name=${const_name}"
+        "-Dinput=${input}"
+        -P "${CMAKE_SOURCE_DIR}/cmake/ConstantBuilderTemplates.cmake"
+    DEPENDS "${CMAKE_SOURCE_DIR}/cmake/ConstantBuilderTemplates.cmake" "${input}"
   )
-  list(APPEND ${outfiles} "${CMAKE_CURRENT_BINARY_DIR}/${name}.c")
+  list(APPEND ${outfiles} "${file_target}")
 endmacro(add_constant_template)
