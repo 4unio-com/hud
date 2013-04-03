48e0de7e6f036b8418c6939a6965246b9ec73f70
Pete Woods <pete.woods@canonical.com>
2013-04-03 17:01:16 +0100

    More tests for hud manager

--- tests/test-hud-manager.c
+++ tests/test-hud-manager.c
@@ -17,7 +17,7 @@ test_manager_create ()
       &appstack_model);
 
   HudManager *manager = hud_manager_new ("test.app");
-  hud_test_utils_process_mainloop (300);
+  hud_test_utils_process_mainloop (100);
 
   dbus_mock_assert_method_call_results (connection, DBUS_NAME, DBUS_PATH,
       "RegisterApplication",
@@ -84,13 +84,73 @@ test_manager_add_actions ()
   hud_test_utils_start_hud_service (&service, &connection, &results_model,
       &appstack_model);
 
+  {
+    DBusMockProperties* properties = dbus_mock_new_properties ();
+    DBusMockMethods* methods = dbus_mock_new_methods ();
+    dbus_mock_methods_append (methods, "AddSources", "a(vso)a(vo)", "", "");
+    dbus_mock_add_object (connection, DBUS_NAME, DBUS_PATH, "/app/object",
+        "com.canonical.hud.Application", properties, methods);
+  }
+
   HudManager *manager = hud_manager_new ("test.app");
-  hud_test_utils_process_mainloop (300);
+  hud_test_utils_process_mainloop (100);
 
-  dbus_mock_assert_method_call_results (connection, DBUS_NAME, DBUS_PATH,
-      "RegisterApplication",
-      "\\(\\[\\(\\d+, \\[<'test.app'>\\]\\)\\],\\)");
+  HudActionPublisher *publisher = hud_action_publisher_new_for_id(NULL);
+  hud_action_publisher_add_action_group(publisher, "app", "/app/object");
+  hud_manager_add_actions(manager, publisher);
+  hud_test_utils_process_mainloop (100);
 
+  dbus_mock_assert_method_call_results (connection, DBUS_NAME, "/app/object",
+        "AddSources",
+        "\\(\\[\\(\\d+, \\[<\\[\\(<-1>, 'app', objectpath '\\/app\\/object'\\)\\]>, <\\[\\(<-1>, objectpath '\\/com\\/canonical\\/hud\\/publisher'\\)\\]>\\]\\)\\],\\)");
+
+  g_object_unref (publisher);
+  g_object_unref (manager);
+
+  hud_test_utils_stop_hud_service (service, connection, results_model,
+      appstack_model);
+}
+
+/**
+ * the remove_actions method does nothing at the moment
+ */
+
+static void
+test_manager_remove_actions ()
+{
+  DbusTestService *service = NULL;
+  GDBusConnection *connection = NULL;
+  DeeModel *results_model = NULL;
+  DeeModel *appstack_model = NULL;
+
+  hud_test_utils_start_hud_service (&service, &connection, &results_model,
+      &appstack_model);
+
+  {
+    DBusMockProperties* properties = dbus_mock_new_properties ();
+    DBusMockMethods* methods = dbus_mock_new_methods ();
+    dbus_mock_methods_append (methods, "AddSources", "a(vso)a(vo)", "", "");
+    dbus_mock_add_object (connection, DBUS_NAME, DBUS_PATH, "/app/object",
+        "com.canonical.hud.Application", properties, methods);
+  }
+
+  HudManager *manager = hud_manager_new ("app.id");
+  hud_test_utils_process_mainloop (100);
+
+  HudActionPublisher *publisher = hud_action_publisher_new_for_id(NULL);
+  hud_action_publisher_add_action_group(publisher, "app", "/app/object");
+  hud_manager_add_actions(manager, publisher);
+  hud_test_utils_process_mainloop (100);
+
+  dbus_mock_assert_method_call_results (connection, DBUS_NAME, "/app/object",
+        "AddSources",
+        "\\(\\[\\(\\d+, \\[<\\[\\(<-1>, 'app', objectpath '\\/app\\/object'\\)\\]>, <\\[\\(<-1>, objectpath '\\/com\\/canonical\\/hud\\/publisher'\\)\\]>\\]\\)\\],\\)");
+  dbus_mock_clear_method_calls(connection, DBUS_NAME, "/app/object");
+
+  hud_manager_remove_actions(manager, publisher);
+  hud_test_utils_process_mainloop (100);
+
+  g_object_unref (publisher);
   g_object_unref (manager);
 
   hud_test_utils_stop_hud_service (service, connection, results_model,
@@ -103,6 +163,7 @@ test_suite (void)
   g_test_add_func ("/hud/hud/manager/create", test_manager_create);
   g_test_add_func ("/hud/hud/manager/create_with_application", test_manager_create_with_application);
   g_test_add_func ("/hud/hud/manager/add_actions", test_manager_add_actions);
+  g_test_add_func ("/hud/hud/manager/remove_actions", test_manager_remove_actions);
 }
 
 int
