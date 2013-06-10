191c76a5eed982d50d3dda3629ece817e9dc72ae
Ted Gould <ted@gould.cx>
2013-06-10 11:41:40 -0500

    When the proxy is null try not to get the name owner

--- libhud-client/connection.c
+++ libhud-client/connection.c
@@ -231,12 +231,16 @@ hud_client_connection_finalize (GObject *object)
 
 /* Called when the HUD service comes on or off the bus */
 static void
-name_owner_changed (GObject * object, GParamSpec * pspec, gpointer user_data)
+name_owner_changed (G_GNUC_UNUSED GObject * object, G_GNUC_UNUSED GParamSpec * pspec, gpointer user_data)
 {
 	HudClientConnection * self = HUD_CLIENT_CONNECTION(user_data);
 	gboolean connected = FALSE;
+	gchar * owner = NULL;
 
-	gchar * owner = g_dbus_proxy_get_name_owner(G_DBUS_PROXY(self->priv->proxy));
+	if (self->priv->proxy != NULL) {
+		owner = g_dbus_proxy_get_name_owner(G_DBUS_PROXY(self->priv->proxy));
+	}
+
 	if (owner != NULL) {
 		connected = TRUE;
 		g_free(owner);
