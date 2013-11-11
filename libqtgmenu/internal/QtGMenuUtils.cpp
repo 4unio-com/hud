97e2bfbbbd583f76f515d23216fdea619a8f63ff
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2013-11-11 13:56:51 +0200

    Added copyright notices.

--- libqtgmenu/internal/QtGMenuUtils.cpp
+++ libqtgmenu/internal/QtGMenuUtils.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Tomlinson <marcus.tomlinson@canonical.com>
+ */
+
 #include <QtGMenuUtils.h>
 
 #undef signals
@@ -5,13 +23,13 @@
 
 using namespace qtgmenu;
 
-static QVariant makeBoolQVariant( GVariant *gvariant )
+static QVariant makeBoolQVariant( GVariant* gvariant )
 {
   bool value = ( g_variant_get_boolean( gvariant ) ? true : false );
   return QVariant::fromValue( value );
 }
 
-static QVariant makeIntQVariant( GVariant *gvariant )
+static QVariant makeIntQVariant( GVariant* gvariant )
 {
   qint32 value = 0;
 
@@ -23,7 +41,7 @@ static QVariant makeIntQVariant( GVariant *gvariant )
   return QVariant::fromValue( value );
 }
 
-static QVariant makeUIntQVariant( GVariant *gvariant )
+static QVariant makeUIntQVariant( GVariant* gvariant )
 {
   quint32 value = 0;
 
@@ -37,21 +55,21 @@ static QVariant makeUIntQVariant( GVariant *gvariant )
   return QVariant::fromValue( value );
 }
 
-static QVariant makeLongLongQVariant( GVariant *gvariant )
+static QVariant makeLongLongQVariant( GVariant* gvariant )
 {
   qint64 value = g_variant_get_int64( gvariant );
   return QVariant::fromValue( value );
 }
 
-static QVariant makeULongLongQVariant( GVariant *gvariant )
+static QVariant makeULongLongQVariant( GVariant* gvariant )
 {
   quint64 value = g_variant_get_uint64( gvariant );
   return QVariant::fromValue( value );
 }
 
-static QVariant makeStringQVariant( GVariant *gvariant )
+static QVariant makeStringQVariant( GVariant* gvariant )
 {
-  const char *string = NULL;
+  const char* string = NULL;
   gsize length = 0;
   string = g_variant_get_string( gvariant, &length );
 
@@ -59,10 +77,10 @@ static QVariant makeStringQVariant( GVariant *gvariant )
   return QVariant::fromValue( value );
 }
 
-static QVariant makeStringListQVariant( GVariant *gvariant )
+static QVariant makeStringListQVariant( GVariant* gvariant )
 {
   int i = 0;
-  const gchar **stringArray = g_variant_get_strv( gvariant, NULL );
+  const gchar* *stringArray = g_variant_get_strv( gvariant, NULL );
   QStringList value;
 
   if( !stringArray )
@@ -74,29 +92,29 @@ static QVariant makeStringListQVariant( GVariant *gvariant )
   return QVariant::fromValue( value );
 }
 
-static QVariant makeVariantQVariant( GVariant *gvariant )
+static QVariant makeVariantQVariant( GVariant* gvariant )
 {
   return QVariant::fromValue( QtGMenuUtils::GVariantToQVariant( gvariant ) );
 }
 
-static QVariant makeByteArrayQVariant( GVariant *gvariant )
+static QVariant makeByteArrayQVariant( GVariant* gvariant )
 {
   QByteArray value;
   GVariantIter iter;
-  guchar *byte;
+  guchar* byte;
 
   g_variant_iter_init( &iter, gvariant );
   while( g_variant_iter_loop( &iter, "y", &byte ) )
-    value.append( *byte );
+    value.append(* byte );
 
   return QVariant::fromValue( value );
 }
 
-static QVariant makeVariantListQVariant( GVariant *gvariant )
+static QVariant makeVariantListQVariant( GVariant* gvariant )
 {
   QVariantList value;
   GVariantIter iter;
-  GVariant *var;
+  GVariant* var;
 
   g_variant_iter_init( &iter, gvariant );
   while( g_variant_iter_loop( &iter, "v", &var ) )
@@ -105,12 +123,12 @@ static QVariant makeVariantListQVariant( GVariant *gvariant )
   return QVariant::fromValue( value );
 }
 
-static QVariant makeVariantMapQVariant( GVariant *gvariant )
+static QVariant makeVariantMapQVariant( GVariant* gvariant )
 {
   QVariantMap value;
   GVariantIter iter;
-  gchar *key;
-  GVariant *var;
+  gchar* key;
+  GVariant* var;
 
   g_variant_iter_init( &iter, gvariant );
   while( g_variant_iter_loop( &iter, "{sv}", &key, &var ) )
