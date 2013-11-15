9c2979d739331b81e0076ee8cd8794308d986928
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2013-11-15 11:40:27 +0200

    Small fix to action trigger.

--- libqtgmenu/internal/QtGActionGroup.cpp
+++ libqtgmenu/internal/QtGActionGroup.cpp
@@ -94,12 +94,16 @@ void QtGActionGroup::TriggerAction( QString action_name, bool checked )
   int name_length = action_name.size() - action_name.indexOf( '.' ) - 1;
   QString action_only = action_name.right( name_length );
 
-  const gchar* action = action_only.toStdString().c_str();
-  const GVariantType* type = g_action_group_get_action_parameter_type( m_action_group, action );
+  const GVariantType* type = g_action_group_get_action_parameter_type( m_action_group,
+      action_only.toStdString().c_str() );
 
   if( type == nullptr )
   {
-    g_action_group_activate_action( m_action_group, action, nullptr );
+    g_action_group_activate_action( m_action_group, action_only.toStdString().c_str(), nullptr );
+  }
+  else
+  {
+    ///!
   }
 }
 
