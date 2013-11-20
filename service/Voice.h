095315606855939310c3ee007adfc59e7b3c29f9
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2013-11-20 14:16:57 +0200

    Voice skeleton code in place.

---
+++ service/Voice.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Tomlinson <marcus.tomlinson@canonical.com>
+ */
+
+#ifndef HUD_SERVICE_VOICE_H_
+#define HUD_SERVICE_VOICE_H_
+
+#include <QObject>
+#include <QSharedPointer>
+
+namespace hud
+{
+namespace service
+{
+
+class ItemStore;
+
+class Voice : public QObject
+{
+Q_OBJECT
+
+public:
+  explicit Voice();
+  virtual ~Voice();
+
+  virtual QString Listen( QSharedPointer< ItemStore > items ) const = 0;
+
+public:
+  using Ptr = QSharedPointer< Voice >;
+};
+
+} // namespace service
+} // namespace hud
+
+#endif // HUD_SERVICE_VOICE_H_
---
+++ service/VoiceImpl.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Marcus Tomlinson <marcus.tomlinson@canonical.com>
+ */
+
+#ifndef HUD_SERVICE_VOICEIMPL_H_
+#define HUD_SERVICE_VOICEIMPL_H_
+
+//#include <UnityVoiceInterface.h>
+#include <service/Voice.h>
+#include <service/ItemStore.h>
+
+#include <QObject>
+#include <QSharedPointer>
+
+namespace hud
+{
+namespace service
+{
+
+class VoiceImpl : public Voice
+{
+Q_OBJECT
+
+public:
+  explicit VoiceImpl();
+  virtual ~VoiceImpl();
+
+  QString Listen( QSharedPointer< ItemStore > items ) const override;
+};
+
+} // namespace service
+} // namespace hud
+
+#endif /* HUD_SERVICE_VOICEIMPL_H_ */
